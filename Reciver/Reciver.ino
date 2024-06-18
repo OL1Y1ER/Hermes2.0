11921f92e2689a9e400de418c6cda46ed8a01941
Olivier de la Porte des Vaux <olivier.dela@PAEDML-LINUX.LOKAL>
2024-06-18 16:35:03 +0200

    almost done

--- Reciver/Reciver.ino
+++ Reciver/Reciver.ino
@@ -1,18 +1,18 @@
 const int pin = 8;
-const int frequency = 25;
+const int frequency = 250;
 void setup() {
   // put your setup code here, to run once:
   pinMode(pin, OUTPUT);
-  Serial.begin(2000000);
+  Serial.begin(9600);
   Sync();
 
 
 }
 
 void loop() {
-  
+  if(digitalRead(pin)){
   Output();
- 
+  }
 }
 
 void Sync(){
@@ -22,17 +22,19 @@ void Sync(){
   pinMode(pin, INPUT);
 }
 
-int counting =0;
+
 //Format of Input?
 String Output(){
+
+  for(int i = 1; i <= 8 ; i++){
   delay(frequency);
- 
+
+  
   Serial.print(digitalRead(pin)); //For printing out
   //return Reformat(digitalRead(pin));
-
+  }
+  Serial.println();
 }
-/*
 String Reformat(String data){
   return data;
 }
-*/
--- Transmiter/Transmiter.ino
+++ Transmiter/Transmiter.ino
@@ -1,60 +1,122 @@
-const int pin = 8; 
+const int pin = 8;
 
 //For SYNCING purposes
-int startZeit =0;
-int stopZeit =0;
+int startZeit = 0;
+int stopZeit = 0;
 int diffTime = 0;
 bool stopp = false;
 
+
+
+String test;
+int MyArray[50];
+int dieLaengeDerNachricht = 0;
+int y;
+int binArray[8];
+
+
+
 void setup() {
   pinMode(pin, INPUT);
-  Serial.begin(2000000);
+  Serial.begin(9600);
 }
 
-int testData[] = {2};
 void loop() {
-  if(diffTime == 0){
+  if (diffTime == 0) {
     Sync();
   }
-  else{
-    SendingMessages(testData);   
-  }  
+  else {
+    //SendingMessages();
+    SaveInArray();
+    
+    for (int i = 0; i <= dieLaengeDerNachricht ; i++) {
+      DeztoBin(i);
+    }
+  }
 }
-
-
-
-void SendingMessages(int text[]){
+/*
+  void SendingMessages(){//int text[]){
   /*
   delay(diffTime);
   digitalWrite(pin, HIGH);
   delay(diffTime);
   digitalWrite(pin, LOW);
-  */
-  //8bit Sending info
-   //Teszing sending some info
-  for(int i = 0; i < 2; i++){
-    for(int j = 0; j < testData[i]; j++){
-      delay(diffTime);
-      digitalWrite(pin, HIGH);
-      
-    }
-    delay(diffTime);
-    digitalWrite(pin, LOW);
-    
+*/
+
+
+//8bit Sending info
+
+
+
+
+void Sync() {
+  if (digitalRead(pin) & !stopp) {
+    startZeit = millis();
+    stopp = true;
+    Serial.println("Synch1");
   }
-   
+  else if (stopp & !digitalRead(pin)) {
+    stopZeit = millis();
+    diffTime = stopZeit - startZeit;
+    pinMode(pin, OUTPUT);
+    stopp = false;
+    Serial.println("Synch2");
+  }
 }
 
+//other half
 
-void Sync(){
-  if(digitalRead(pin)&!stopp){
-    startZeit = millis();
-    stopp =true;
+
+
+
+
+void SaveInArray() {
+  Serial.println("SiA");
+
+  long lastB;
+  dieLaengeDerNachricht = 0;
+  boolean ende = false;
+  while (Serial.available() <= 0) {
+
   }
-  else if(stopp & !digitalRead(pin)){
-    stopZeit = millis();
-    diffTime = stopZeit-startZeit;
-    pinMode(pin,OUTPUT);
-    stopp = false;
+  while (ende == false) {
+    if (Serial.available() > 0) {
+      MyArray[dieLaengeDerNachricht] = Serial.read();
+      Serial.println(MyArray[dieLaengeDerNachricht]);
+      dieLaengeDerNachricht++;
+      lastB = millis();
+    } else {
+      if (millis() - lastB >= 10000) {
+        ende = true;
+      }
+    }
+
   }
+  dieLaengeDerNachricht--;
+
+}
+
+void DeztoBin(int ArrayLocation) {
+  int dez = MyArray[ArrayLocation];
+  for (int i = 7 ; i >= 0; i--) {
+    y = dez % 2;
+    binArray[i] = y;
+    dez = dez / 2;
+  }
+  /*prints bin
+  Serial.println();
+  for (int j = 0; j <= 7; j++) {
+    Serial.print(binArray[j]);
+  }
+  */
+  digitalWrite(pin, HIGH);
+  digitalWrite(pin, LOW);
+  Serial.println();
+  //ubertragung();
+  for (int i = 1; i <= 8; i++) {
+    delay(diffTime);
+  
+    digitalWrite(pin, binArray[i]);
+  }
+  
 }
